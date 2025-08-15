f81af47de18f1cb1386f5369bc107f108dce7e29
lucky <lucky.zsp1998@gmail.com>
2025-08-15 11:31:57 +0800

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,7 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/alpha/",
   plugins: [
     vue(),
     vueDevTools(),
